630bb8d5938fdadc22ae726e6e06120b90949c42
Trishawna Ramdeen <trishawna.ramdeen@mymona.uwi.edu>
2018-11-11 20:00:32 +0000

    Exercise 2 completed ish

--- lab6.js
+++ lab6.js
@@ -1,17 +1,12 @@
 window.onload = function(){
-    var searchb = document.getElementById("searchb");
-    searchb.addEventListener("onclick", function(){
-    var xml = new  XMLHttpRequest();
+    document.getElementById("searchb").addEventListener("click", function(){
+       let xml = new  XMLHttpRequest();
        xml.onreadystatechange = function(){
-           if(xml.status === 200 && xml.readyState === 4 ){
+           if(xml.readyState === 4 && xml.status === 200){
                alert(xml.responseText);
-           }else{
-               alert("There's a problem with the request");
            }
        };
        xml.open("GET","request.php?q=definition",true);
        xml.send();
     });
-    
-    
 }; 
