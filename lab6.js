96abffbfc19e10cb7a1e6d1f3f916e388555a5d3
Trishawna Ramdeen <trishawna.ramdeen@mymona.uwi.edu>
2018-11-13 01:45:36 +0000

    Exercise 4

--- lab6.js
+++ lab6.js
@@ -1,18 +1,34 @@
 window.onload = function(){
-    var search= document.getElementById("searchb");
-    var searchinput= document.getElementById("search"); 
-    var searchall= document.getElementById("searchall");
-    var res= document.getElementById("result");
-    search.addEventListener("click", function(){
-       let xhttp = new  XMLHttpRequest();
+    let searchinput = document.getElementById("searchinput");//inputsearch
+    let res = document.getElementById("result");//result
+    let search =  document.getElementById("searchb");//searchbutton
+   search.addEventListener("click", function(){
+    let searchA =  document.getElementById("searchb"); //searchbutton
+    let searchAll =  document.getElementById("searchAll"); //seachALL
+   searchA.addEventListener("click", function(){
+       let xhttp = new XMLHttpRequest();
        xhttp.onreadystatechange = function(){
            if(xhttp.readyState === 4 && xhttp.status === 200){
-               //alert(xhttp.responseText);
-               res.innerHTML = xhttp.responseText;
-           }
-       };
-      // xhttp.open("GET","request.php?q="+"definition",true);
+
        xhttp.open("GET","request.php?q="+searchinput.value,true);
        xhttp.send();
     });
-}; 
+    searchAll.addEventListener("click", function(){
+       var xhttp = new XMLHttpRequest();
+       xhttp.onreadystatechange = function(){
+           if(xhttp.readyState === 4 && xhttp.status === 200){
+               var output = "<ol>";
+               var text = xhttp.responseText;
+               var terms = text.getElementsByTagName("definition");
+               for(var i = 0; i<terms.length;i++){
+                   output+="<li><h3>"+terms[i].getAttribute("name")+"</h3><p>"+terms[i].childNodes[0].nodeValue+
+                   "</p><p> -"+terms[i].getAttribute("author")+"</p></li>";
+               }
+               output+="</ol>";
+               res.innerHTML ="<h2>Result</h2>"+output;
+           }
+       };
+       xhttp.open("GET","request.php?q=&all=true",true);
+       xhttp.send();
+    });
+};
