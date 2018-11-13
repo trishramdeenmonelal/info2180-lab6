a0e3f2499a2caba220de666560f907551b373a9b
Trishawna Ramdeen <trishawna.ramdeen@mymona.uwi.edu>
2018-11-13 20:36:00 +0000

    Exercise 3 FINALLL

--- lab6.js
+++ lab6.js
@@ -1,34 +1,19 @@
 window.onload = function(){
-    let searchinput = document.getElementById("searchinput");//inputsearch
-    let res = document.getElementById("result");//result
-    let search =  document.getElementById("searchb");//searchbutton
-   search.addEventListener("click", function(){
-    let searchA =  document.getElementById("searchb"); //searchbutton
-    let searchAll =  document.getElementById("searchAll"); //seachALL
-   searchA.addEventListener("click", function(){
-       let xhttp = new XMLHttpRequest();
-       xhttp.onreadystatechange = function(){
-           if(xhttp.readyState === 4 && xhttp.status === 200){
-
-       xhttp.open("GET","request.php?q="+searchinput.value,true);
-       xhttp.send();
-    });
-    searchAll.addEventListener("click", function(){
-       var xhttp = new XMLHttpRequest();
-       xhttp.onreadystatechange = function(){
-           if(xhttp.readyState === 4 && xhttp.status === 200){
-               var output = "<ol>";
-               var text = xhttp.responseText;
-               var terms = text.getElementsByTagName("definition");
-               for(var i = 0; i<terms.length;i++){
-                   output+="<li><h3>"+terms[i].getAttribute("name")+"</h3><p>"+terms[i].childNodes[0].nodeValue+
-                   "</p><p> -"+terms[i].getAttribute("author")+"</p></li>";
-               }
-               output+="</ol>";
-               res.innerHTML ="<h2>Result</h2>"+output;
+    var searchb= document.getElementById("searchb");
+    var search= document.getElementById("searchinput");
+   
+    var res= document.getElementById("result");
+    searchb.addEventListener("click", function(){
+       let xhttp = new  XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if(xhttp.readyState === 4 && xhttp.status === 200){
+                //alert(xhttp.responseText);
+               res.innerHTML = xhttp.responseText;
            }
        };
-       xhttp.open("GET","request.php?q=&all=true",true);
+       xhttp.open("GET","request.php?q="+"definition",true);
+      // xhttp.open("GET","request.php?q="+"definition",true);
+       xhttp.open("GET","request.php?q="+search.value,true);
        xhttp.send();
     });
-};
+}; 
